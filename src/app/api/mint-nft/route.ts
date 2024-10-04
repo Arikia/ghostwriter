c22b4d3ee5d8743584d43a39f0630c3a139ebf34
julibi <hyunkyungyi43@gmail.com>
2024-10-04 12:00:24 +0200

    use irys for image upload to arweave & prepare metadata

--- src/app/api/mint-nft/route.ts
+++ src/app/api/mint-nft/route.ts
@@ -1,4 +1,5 @@
 import {
+  createGenericFile,
   generateSigner,
   createSignerFromKeypair,
   signerIdentity,
@@ -7,32 +8,40 @@ import {
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { create, fetchCollection } from "@metaplex-foundation/mpl-core";
 import { base58 } from "@metaplex-foundation/umi/serializers";
-import { config } from "dotenv";
+import {
+  irysUploader,
+  // @ts-ignore "type definitions are missing"
+} from "@metaplex-foundation/umi-uploader-irys";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { Keypair } from "@solana/web3.js";
+import fs from "fs";
+import path from "path";
 
-// Load environment variables from .env file
-config();
+/* TODO:
+-hash the text and save that to metadata
+-set correct metadata 
+-protect the route with a secret key or sth.
+-validate the request body
+-what's with batch uploads?
+*/
 
 export async function POST(req: NextRequest) {
   if (req.method === "POST") {
-    const { uri, name } = await req.json();
+    const { name } = await req.json(); // what to include in request body
 
-    // JUST FOR JULES
     // const secretKey = new Uint8Array(
     //   process.env
     //     .DEV_WALLET_SECRET_KEY!.replace("[", "")
     //     .replace("]", "")
     //     .split(",")
     //     .map(Number)
-    // ); // Parse the secret key
+    // );
     // const keypair = Keypair.fromSecretKey(secretKey); // Create the Keypair
     // const ourPublicKey = keypair.publicKey.toString(); // Get the public key
     // console.log("Public Key:", ourPublicKey);
 
     // Validate required fields
-    if (!uri || !name) {
+    if (!name) {
       return NextResponse.json(
         { error: "Missing required fields" },
         { status: 400 }
@@ -69,7 +78,7 @@ export async function POST(req: NextRequest) {
         new Uint8Array(secretKey)
       );
       const adminSigner = createSignerFromKeypair(umi, keypair);
-      umi.use(signerIdentity(adminSigner));
+      umi.use(signerIdentity(adminSigner)).use(irysUploader());
 
       // Generate the Asset KeyPair
       const asset = generateSigner(umi);
@@ -80,14 +89,63 @@ export async function POST(req: NextRequest) {
         umi,
         publicKey(process.env.COLLECTION_PUBKEY)
       );
-      console.log(collection);
+
+      // Upload ctrl-x icon to arweave
+      let imageUri = process.env.NFT_IMAGE_AW_URL;
+      if (!imageUri) {
+        const imageFile = fs.readFileSync(
+          path.join(process.cwd(), "public", "ctrlxicon.png")
+        );
+
+        const umiImageFile = createGenericFile(imageFile, "ctrl-x.png", {
+          tags: [{ name: "Content-Type", value: "image/png" }],
+        });
+
+        // upload image to Arweave via Irys and get returned an uri
+        // address where the file is located -> imageUri[0]
+        const uri = await umi.uploader.upload([umiImageFile]).catch((err) => {
+          throw new Error(err);
+        });
+        imageUri = "https://gateway.irys.xyz/" + uri[0].split("/").pop(); // https://arweave.net/id currently not working
+      }
+
+      // Generate Metadata
+      const metadata = {
+        name: "CTRL+X",
+        description: name,
+        image: imageUri,
+        external_url: "https://example.com",
+        attributes: [
+          {
+            trait_type: "trait1",
+            value: "value1",
+          },
+          {
+            trait_type: "trait2",
+            value: "value2",
+          },
+        ],
+        properties: {
+          files: [
+            {
+              uri: imageUri,
+              type: "image/png",
+            },
+          ],
+          category: "image",
+        },
+      };
+
+      // Upload Metadata
+      const metadataUri = await umi.uploader.uploadJson(metadata);
+      console.log({ metadataUri });
 
       // Generate the Asset
       const tx = await create(umi, {
         asset,
         collection,
         name: `${name} (Journalistic Content recorded through CTRL+X)`,
-        uri,
+        uri: metadataUri,
       }).sendAndConfirm(umi);
 
       // Deserialize the Signature from the Transaction
@@ -100,6 +158,8 @@ export async function POST(req: NextRequest) {
         { status: 200 }
       );
     } catch (error) {
+      // @ts-ignore
+      console.log(error.message);
       return NextResponse.json({ error: "Mint failed" }, { status: 500 });
     }
   } else {
